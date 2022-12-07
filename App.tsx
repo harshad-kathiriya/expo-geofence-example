2b0d7cd3e4e7d829ce0f23721f2c31c1c21ede2a
Harshad Kathiriya <harshad.k@neuralmetrics.ai>
2022-12-07 14:05:50 +0530

    Configured location service

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import * as TaskManager from 'expo-task-manager';
 import * as Location from 'expo-location';
-import { GEOFENCE_TASK } from './constantList';
 import React, { useEffect, useState } from 'react';
+
+const GEOFENCE_TASK = "geofence-task";
+const LOCATION_API_TASK = "location-api-task";
 
 
 TaskManager.defineTask(GEOFENCE_TASK, ({ data:{eventType, region}, error }) => {
   if (error) {
-    console.log("Geofence error", error);
+    console.log(`Task - ${GEOFENCE_TASK} - error`, error);
     return;
   }
   if (eventType === Location.GeofencingEventType.Enter) {
@@ -19,36 +21,60 @@ TaskManager.defineTask(GEOFENCE_TASK, ({ data:{eventType, region}, error }) => {
   }
 });
 
+TaskManager.defineTask(LOCATION_API_TASK, ({ data: { locations }, error }) => {
+  if (error) {
+    console.log(`Task - ${LOCATION_API_TASK} - error`, error);
+    return;
+  }
+ // console.log('Received new locations', locations);
+ });
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   
-  useEffect(() => {
-    const configureGeoFence = async() => {
-      const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
-      if (foregroundStatus === 'granted') {
-        const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
-        if (backgroundStatus === 'granted') {
-          let region = [{identifier: "Hudson Street", latitude:40.7354, longitude:-74.0059, radius:20,  notifyOnEnter: true,
-          notifyOnExit: true,}];      
-          if (!TaskManager.isTaskDefined(GEOFENCE_TASK)) {
+  const configureGeoFence = async() => {
+    let getAllTasks = await TaskManager.getRegisteredTasksAsync();
+    console.log("getAllTasks", JSON.stringify(getAllTasks[0]));
+    console.log("TaskManager.isTaskDefined('GEOFENCE_TASK')", TaskManager.isTaskDefined(GEOFENCE_TASK));
+    
+    const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+    if (foregroundStatus === 'granted') {
+      const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+      if (backgroundStatus === 'granted') {
+        let region = [{identifier: "Hudson Street", latitude:40.7354, longitude:-74.0059, radius:100,  notifyOnEnter: true,
+        notifyOnExit: true,}];      
+        if (TaskManager.isTaskDefined(GEOFENCE_TASK)) {
             console.log("Task registered");
-            await Location.startGeofencingAsync(GEOFENCE_TASK, region);
-          }
-          setIsLoading(false);
+          await Location.startGeofencingAsync(GEOFENCE_TASK, region);
         }
-      }
-    };
-    configureGeoFence();
-  },[]);
+        if (TaskManager.isTaskDefined(LOCATION_API_TASK)) {            
+          await Location.startLocationUpdatesAsync(LOCATION_API_TASK, {
+            accuracy: Location.Accuracy.BestForNavigation
+          }); 
+        }
+        setIsLoading(false);
+      }               
+    }
+  };
+   
+  const PermissionsButton = () => (
+    <View style={styles.buttonContainer}>
+      <Button onPress={configureGeoFence} title="Enable background location" />
+    </View>
+  );
 
-  
   return (
     <View style={styles.container}>
+      
+      <PermissionsButton />
       {
-        isLoading ? <Text>Configuring geo-fence</Text>
+        isLoading ? <></>
         : 
-          <Text>geo-fence started</Text> 
+        <View>
+          <View>
+            <Text>geo-fence started</Text>
+          </View>         
+        </View>
       }
       </View>
       
@@ -62,4 +88,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    flex: 0.5
+  }
 });
--- constantList.ts
+++
@@ -1,1 +0,0 @@
-export const GEOFENCE_TASK = "geofence-task";
